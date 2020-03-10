a07aff6c45d914c59b1f118ec66743a4369cadf6
Jamison Engels <engelsj@tcd.ie>
2020-03-10 14:31:07 +0000

    Completed the sidebar component

--- gui/webspace_ng/static/views.js
+++ gui/webspace_ng/static/views.js
@@ -1,5 +1,5 @@
 Vue.component('NotFound', {
-  template: `
+    template: `
     <div>
       <h1>Not Found</h1>
       <p class="lead">Couldn't find that page, sorry.</p>
@@ -9,7 +9,7 @@ Vue.component('NotFound', {
 });
 
 Vue.component('HomeView', {
-  template: `
+    template: `
     <div>
       <h1>Home</h1>
       <p class="lead">Hello, world!</p>
@@ -17,22 +17,59 @@ Vue.component('HomeView', {
   `
 });
 
+Vue.component('Navbar', {
+    template: `
+    <div>
+    <div class="sidenav">
+      <i class="fa fa-home fa-lg"> <a href="#"> Dashboard </a></i>
+      <i class="fa fa-terminal fa-lg"> <a href="#"> Console </a></i>
+      <i class="fa fa-cog fa-lg"> <a href="#"> Configs </a></i>
+      <i class="fa fa-globe fa-lg"> <a href="#"> Domains </a></i>
+      <i class="fa fa-plug fa-lg"> <a href="#"> Ports </a></i>
+    </div>
+    </div>
+  `
+});
+
 Vue.component('Dashboard', {
   template: `
-    <div>
-      <div class="sidenav">
-        <i class="fa fa-home fa-lg"> <a href="#"> Dashboard </a></i>
-        <i class="fa fa-terminal fa-lg"> <a href="#"> Console </a></i>
-        <i class="fa fa-cog fa-lg"> <a href="#"> Configs </a></i>
-        <i class="fa fa-globe fa-lg"> <a href="#"> Domains </a></i>
-        <i class="fa fa-plug fa-lg"> <a href="#"> Ports </a></i>
-      </div>
-    </div> `
+  <navbar/>
+`
 });
 
 
+Vue.component('Welcome', {
+    template: `
+    <h1 style = "text-align: center"> Welcome to your dashboard </h1> 
+    <h4 style = "text-align: center"> Setup your environment to use your webspace </h4> 
+    <button class = "center"type = "button"> Get Started </button>
+    <footer style = "text-align: center"> Brought to you by DU Netsoc </footer>
+  `
+});
+
+
+Vue.component('Operating System', {
+    template: `
+    <div class = "container">
+      <div class = "row">
+        <div class = "col"> Ubuntu </div> 
+        <div class = "col"> Arch </div> 
+        <div class = "col"> Redfin </div> 
+      </div> 
+      <br>
+      <div class = "row">
+        <div class = "col"> Fedora </div> 
+        <div class = "col"> Centos </div> 
+        <div class = "col"> Alpine </div> 
+      </div>
+    </div> 
+    <button class = "center"type = "button"> Next </button> 
+    <footer style = "text-align: center"> Brought to you by DU Netsoc</footer>
+  `
+});
+
 Vue.component('Login', {
-  template: `
+    template: `
   <template>
     <div class ="center">
       <img class="center" src="/static/images/logo.jpg" alt="Netsoc Logo">
