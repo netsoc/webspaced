4c3ad84a8bea095782a9d6eda3a895f016240434
Jack O'Sullivan <jackos1998@gmail.com>
2020-03-25 01:41:31 +0000

    webspaced: Fix empty Redis Traefik configuration

--- webspaced/traefik-entrypoint.sh
+++ webspaced/traefik-entrypoint.sh
@@ -8,4 +8,7 @@ until redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" ping; do
     sleep 0.1
 done
 
+# We need a dummy Traefik config so the configuration is never empty
+redis-cli set traefik/http/middlewares/dummy/redirectScheme/scheme 'https'
+
 exec traefik "--providers.redis.endpoints=$REDIS_HOST:$REDIS_PORT" "$@"
