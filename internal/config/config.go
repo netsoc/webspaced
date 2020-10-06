8bbd93e60cb1d4ee41bc16f632f45e8cef75b5b8
Jack O'Sullivan <jackos1998@gmail.com>
2020-10-06 20:39:01 +0100

    Fix LogLevel parsing from config

--- internal/config/config.go
+++ internal/config/config.go
@@ -15,7 +15,7 @@ import (
 // StringToLogLevelHookFunc returns a mapstructure.DecodeHookFunc which parses a logrus Level from a string
 func StringToLogLevelHookFunc() mapstructure.DecodeHookFunc {
 	return func(f reflect.Type, t reflect.Type, data interface{}) (interface{}, error) {
-		if f.Kind() != reflect.String || t.Kind() != reflect.TypeOf(log.InfoLevel).Kind() {
+		if f.Kind() != reflect.String || t != reflect.TypeOf(log.InfoLevel) {
 			return data, nil
 		}
 
